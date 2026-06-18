e5b2eebb0d6590eb8511f46118f13b0e6ec0b08a
harikrishnay2020-alt <harikrishnay2020@gmail.com>
2026-06-18 14:18:28 +0530

    my-react-app

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import JSX from './events.jsx'
+import './index.css'
+import JSX from './student.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
